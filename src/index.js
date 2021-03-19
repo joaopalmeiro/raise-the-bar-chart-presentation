4b28c736b23cfe9db8450f99851621055e4f8de5
João Palmeiro <joaommpalmeiro@gmail.com>
2021-03-19 13:02:01 +0000

    Update slides and snippets

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from "victory";
-
 import {
-  FlexBox,
-  Heading,
-  FullScreen,
   Appear,
-  Slide,
-  Deck,
-  Text,
   Box,
+  Deck,
+  FlexBox,
+  FullScreen,
+  Heading,
+  ListItem,
+  Slide,
+  Text,
+  UnorderedList,
 } from "spectacle";
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme } from "victory";
 
 // Source: https://www.nordtheme.com/ (by Arctic Ice Studio & Sven Greb)
 const colors = {
@@ -114,6 +115,7 @@ const Presentation = () => (
         </Text>
       </FlexBox>
     </Slide>
+
     <Slide>
       {/* Theme: https://github.com/FormidableLabs/victory/blob/main/packages/victory-core/src/victory-theme/material.js */}
       <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
@@ -149,6 +151,7 @@ const Presentation = () => (
         />
       </VictoryChart>
     </Slide>
+
     <Slide>
       <Heading>“It's Over 9000!”</Heading>
       <Text>
@@ -170,6 +173,7 @@ const Presentation = () => (
       <Text>🧑‍🤝‍🧑 C. Xiong, C. Ceja, C. Ludwig, S. Franconeri</Text>
       <Text>🗓️ 2020</Text>
     </Slide>
+
     <Slide>
       <Heading>“It's not you, it's me”</Heading>
       <Text>
@@ -179,6 +183,7 @@ const Presentation = () => (
       <Text>🧑‍🤝‍🧑 C. Ceja, C. McColeman, C. Xiong, S. Franconeri</Text>
       <Text>🗓️ 2021</Text>
     </Slide>
+
     <Slide>
       <Heading>“No pajamas all day”</Heading>
       <Text>
@@ -188,6 +193,7 @@ const Presentation = () => (
       <Text>🧑‍🤝‍🧑 D. Skau, R. Kosara</Text>
       <Text>🗓️ 2017</Text>
     </Slide>
+
     <Slide>
       <Heading>“Tower of Pisa yes, italics no”</Heading>
       <Text>
@@ -196,6 +202,7 @@ const Presentation = () => (
       <Text>🧑‍🤝‍🧑 L. Rello, R. Baeza-Yates</Text>
       <Text>🗓️ 2013</Text>
     </Slide>
+
     <Slide>
       <Heading>Truncating the Y-Axis</Heading>
       <Appear>
@@ -211,6 +218,16 @@ const Presentation = () => (
       </Appear>
       <Appear>
         <Text>
+          Although subjective perception is affected, nothing has been
+          falsified.
+        </Text>
+      </Appear>
+    </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Appear>
+        <Text>
           Umbrella question: What is the{" "}
           <span style={underline}>
             impact on subjective assessments of data
@@ -218,30 +235,201 @@ const Presentation = () => (
           across multiple charts?
         </Text>
       </Appear>
-    </Slide>
-    <Slide>
-      <Heading>Truncating the Y-Axis</Heading>
       <Appear>
-        <Text>Question: HERE</Text>
+        <Text>
+          Is the impact of Y-axis truncation different between bar and line
+          charts?
+        </Text>
+      </Appear>
+      <Appear>
+        <Text>
+          Does explicitly indicating that Y-axis truncation has taken place
+          reduce the introduced bias?
+        </Text>
       </Appear>
     </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Text>They conducted 3 crowdsourced online experiments.</Text>
+      <Appear>
+        <Text>"Which value is larger, the first value or the last value?"</Text>
+      </Appear>
+      <Appear>
+        <Text>
+          "Subjectively, how different is the first value compared to the last
+          value?"
+        </Text>
+      </Appear>
+    </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Text>
+        <span style={underline}>Experiment 1</span> (framing interventions):
+      </Text>
+      <UnorderedList>
+        <ListItem>2 charts types (bar and line)</ListItem>
+        <Appear>
+          <ListItem>Value-based and trend-based questions</ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>3 truncation levels (0%, 25%, 50%)</ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>2 slope levels (12.5%, 25%)</ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>2 or 3 data values</ListItem>
+        </Appear>
+      </UnorderedList>
+    </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Text>
+        <span style={underline}>Main conclusions</span> (statistically
+        speaking):
+      </Text>
+      <UnorderedList>
+        <ListItem>
+          Increasing the starting point of the Y-axis results in larger
+          perceived severity in effect size
+        </ListItem>
+        <Appear>
+          <ListItem>
+            Neither the chart type (bar or line) nor the method of soliciting
+            the perceived severity (value or trend) produced significant
+            differences
+          </ListItem>
+        </Appear>
+      </UnorderedList>
+    </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Text>
+        <span style={underline}>Experiment 2</span> (visual design
+        interventions):
+      </Text>
+      <UnorderedList>
+        <ListItem>
+          Assess the impact of visual design elements in bar charts that
+          indicate truncation on perceived effect size
+        </ListItem>
+        <Appear>
+          <ListItem>
+            3 charts types (bar, broken axis bar, gradient bar)
+          </ListItem>
+        </Appear>
+      </UnorderedList>
+    </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Text>
+        <span style={underline}>Main conclusions</span> (statistically
+        speaking):
+      </Text>
+      <UnorderedList>
+        <ListItem>
+          There was no significant difference in the perceived severity between
+          these designs
+        </ListItem>
+        <Appear>
+          <ListItem>
+            In other words, increased axis truncation resulted in similar
+            increases in perceived severity
+          </ListItem>
+        </Appear>
+      </UnorderedList>
+    </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Text>
+        <span style={underline}>Experiment 3</span> (bias in value estimation):
+      </Text>
+      <UnorderedList>
+        <ListItem>
+          Does truncating the Y-axis not only result in a qualitative increase
+          in effect sizes but also in misreading the values?
+        </ListItem>
+        <Appear>
+          <ListItem>
+            Does attending to the numbers reduce this exaggeration?
+          </ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>"What is the value of the first bar (0-100%)"</ListItem>
+        </Appear>
+      </UnorderedList>
+    </Slide>
+
+    <Slide>
+      <Heading>Truncating the Y-Axis</Heading>
+      <Text>
+        <span style={underline}>Main conclusions</span> (statistically
+        speaking):
+      </Text>
+      <UnorderedList>
+        <ListItem>
+          Perceived severity was significantly different across levels of
+          truncation
+        </ListItem>
+        <Appear>
+          <ListItem>
+            The error in estimating trend was not significantly different across
+            levels of truncation
+          </ListItem>
+        </Appear>
+        <Appear>
+          <ListItem>
+            However, the error in estimating individual values was significantly
+            different (25% start)
+          </ListItem>
+        </Appear>
+      </UnorderedList>
+    </Slide>
+
+    <Slide>
+      <Heading>Takeaways</Heading>
+      <Appear>
+        <Text>
+          We cannot rely on visual indicators of truncated axes — the subjective
+          impact of axis truncation is persistent across visualizations designs
+        </Text>
+      </Appear>
+      <Appear>
+        <Text>
+          We have a great deal of control over the perceived effect size — we
+          have to take into account what we want to communicate at a per-data
+          and per-task level
+        </Text>
+      </Appear>
+    </Slide>
+
     <Slide>
       <Heading>
         Biased Average Position Estimates in Line and Bar Graphs
       </Heading>
     </Slide>
+
     <Slide>
       <Heading>Aspect Ratio Biases Recall of Position Encodings</Heading>
       <Appear>
         <Text>The experimental setup is similar to the previous one.</Text>
       </Appear>
     </Slide>
+
     <Slide>
       <Heading>Precision in Pictorial Bar Charts</Heading>
     </Slide>
+
     <Slide>
       <Heading>Good Fonts for Dyslexia</Heading>
     </Slide>
+
     <Slide>
       <FlexBox height="100%" flexDirection="column">
         <Heading margin="0px" fontSize="150px">
